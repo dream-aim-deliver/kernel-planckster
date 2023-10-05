0c6de4366235cf0a7e5492a9c1566efca88d23d7
Mayank Sharma <imptodefeat@gmail.com>
2023-10-06 01:34:28 +0200

    setup: sqlalchemy and alembic Fixes #1
    - Swapped names kp-test and kp-db
    - Fixed type errors
    - db: fixed pg connection errors

--- rest/config.py
+++ rest/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Settings(BaseSettings):
+class Settings(BaseSettings):  # type: ignore
     host: str
     port: int
     model_config = SettingsConfigDict(env_file="../.env")
