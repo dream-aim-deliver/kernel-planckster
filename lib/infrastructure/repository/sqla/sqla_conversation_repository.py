a77b2c3c8e9a9662f59b461313c9b647a7879311
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2023-10-07 01:10:31 +0200

    sqla: first version of SQLAConversationRepository (not tested yet)
    Implemented features:
    - new_conversation
    - get_conversation_researchcontext
    - list_conversation_messages
    - update_conversation
    - list_conversation_sources

--- lib/infrastructure/repository/sqla/sqla_conversation_repository.py
+++ lib/infrastructure/repository/sqla/sqla_conversation_repository.py
@@ -1,31 +1,25 @@
-from distutils import core
 from typing import List
 from lib.core.dto.conversation_repository_dto import (
     ConversationDTO,
     GetConversationResearchContextDTO,
     ListConversationMessagesDTO,
+    ListConversationSourcesDTO,
 )
 from lib.core.entity.models import (
-    LLM,
-    Conversation,
-    EmbeddingModel,
     MessageBase,
     MessageQuery,
     MessageResponse,
     ResearchContext,
     SourceData,
     TMessageBase,
-    User,
 )
 from lib.core.ports.secondary.conversation_repository import ConversationRepository
 from lib.infrastructure.repository.sqla.database import TDatabaseFactory
 from lib.infrastructure.repository.sqla.models import (
-    SQLALLM,
     SQLAConversation,
     SQLAMessageQuery,
     SQLAMessageResponse,
     SQLAResearchContext,
-    SQLAUser,
 )
 
 
@@ -42,8 +36,10 @@ class SQLAConversationRepository(ConversationRepository):
 
         @param research_context_id: The ID of the research context to create the conversation in.
         @type research_context_id: int
+        @param conversation_title: The title of the conversation.
+        @type conversation_title: str
         @return: A DTO containing the result of the operation.
-        @rtype: SuccessConversationDTO | ErrorConversationDTO
+        @rtype: ConversationDTO
         """
         if research_context_id is None:
             errorDTO = ConversationDTO(
@@ -52,6 +48,17 @@ class SQLAConversationRepository(ConversationRepository):
                 errorMessage="Research Context ID cannot be None",
                 errorName="Research Context ID not provided",
                 errorType="ResearchContextIdNotProvided",
+            )
+            self.logger.error(f"{errorDTO}")
+            return errorDTO
+
+        if conversation_title is None:
+            errorDTO = ConversationDTO(
+                status=False,
+                errorCode=-1,
+                errorMessage="Conversation title cannot be None",
+                errorName="Conversation title not provided",
+                errorType="ConversationTitleNotProvided",
             )
             self.logger.error(f"{errorDTO}")
             return errorDTO
@@ -81,12 +88,14 @@ class SQLAConversationRepository(ConversationRepository):
         try:
             sqla_new_conversation.save(session=self.session)
             new_conversation = self.session.query(SQLAConversation).filter_by(id=sqla_new_conversation.id)[-1]
-            return ConversationDTO(status="success", conversation_id=new_conversation.id)
+
+            return ConversationDTO(status=True, conversation_id=new_conversation.id)
+
         except Exception as e:
             self.logger.error(f"Error while creating new conversation: {e}")
             errorDTO = ConversationDTO(
                 status=False,
-                errorCode=-1,  # TODO: is this code ok?
+                errorCode=-1,
                 errorMessage=f"Error while creating new conversation: {e}",
                 errorName="Error while creating new conversation",
                 errorType="ErrorWhileCreatingNewConversation",
@@ -94,18 +103,16 @@ class SQLAConversationRepository(ConversationRepository):
             self.logger.error(f"{errorDTO}")
             return errorDTO
 
-<<<<<<< HEAD
-        return ConversationDTO(status="success")
-
     def get_conversation_research_context(self, conversation_id: int) -> GetConversationResearchContextDTO:
-        """ """
-=======
-    def get_conversation_research_context(self, conversation_id: int) -> GetConversationResearchContextDTO:
-        """ """
+        """
+        Gets the research context of a conversation.
 
-    def get_conversation(self, conversation_id: int) -> GetConversationDTO[TMessageBase] | ErrorConversationDTO:
-        """ """
->>>>>>> bd17aa7 (sqla: return id of newly created conversation #13)
+        @param research_context_id: The ID of the research context to get the conversation for.
+        @type research_context_id: int
+        @return: A DTO containing the result of the operation.
+        @rtype: GetConversationResearchContextDTO
+        """
+
         if conversation_id is None:
             errorDTO = GetConversationResearchContextDTO(
                 status=False,
@@ -164,10 +171,16 @@ class SQLAConversationRepository(ConversationRepository):
             status=True,
             data=core_research_context,
         )
-<<<<<<< HEAD
+
 
     def list_conversation_messages(self, conversation_id: int) -> ListConversationMessagesDTO[TMessageBase]:
-        """ """
+        """
+        Lists all messages in a conversation.
+
+        @param conversation_id: The ID of the conversation to list messages for.
+        @type conversation_id: int
+        @return: A DTO containing the result of the operation.
+        """
         if conversation_id is None:
             errorDTO = ListConversationMessagesDTO[TMessageBase](
                 status=False,
@@ -225,5 +238,161 @@ class SQLAConversationRepository(ConversationRepository):
             status=True,
             data=core_messages,
         )
-=======
->>>>>>> bd17aa7 (sqla: return id of newly created conversation #13)
+
+
+    def update_conversation(self, conversation_id: int, conversation_title: str) -> ConversationDTO:
+        """
+        Updates a conversation in the research context.
+
+        @param conversation_id: The ID of the conversation to update.
+        @type conversation_id: int
+        @param conversation_title: The title of the conversation.
+        @type conversation_title: str
+        @return: A DTO containing the result of the operation.
+        @rtype: ConversationDTO
+        """
+        if conversation_id is None:
+            errorDTO = ConversationDTO(
+                status=False,
+                errorCode=-1,
+                errorMessage="Conversation ID cannot be None",
+                errorName="Conversation ID not provided",
+                errorType="ConversationIdNotProvided",
+            )
+            self.logger.error(f"{errorDTO}")
+            return errorDTO
+
+        if conversation_title is None:
+            errorDTO = ConversationDTO(
+                status=False,
+                errorCode=-1,
+                errorMessage="Conversation title cannot be None",
+                errorName="Conversation title not provided",
+                errorType="ConversationTitleNotProvided",
+            )
+            self.logger.error(f"{errorDTO}")
+            return errorDTO
+
+        sqla_conversation: SQLAConversation | None = (
+            self.session.query(SQLAConversation).filter_by(id=conversation_id).first()
+        )
+
+        if sqla_conversation is None:
+            self.logger.error(f"Conversation with ID {conversation_id} not found in the database.")
+            errorDTO = ConversationDTO(
+                status=False,
+                errorCode=-1,
+                errorMessage=f"Conversation with ID {conversation_id} not found in the database.",
+                errorName="Conversation not found",
+                errorType="ConversationNotFound",
+            )
+            self.logger.error(f"{errorDTO}")
+            return errorDTO
+
+        try:
+            sqla_conversation.update({"title": conversation_title})
+
+            updated_conversation = self.session.query(SQLAConversation).filter_by(id=sqla_conversation.id)[-1]
+
+            return ConversationDTO(status=True, conversation_id=updated_conversation.id)
+
+        except Exception as e:
+            self.logger.error(f"Error while updating the conversation: {e}")
+            errorDTO = ConversationDTO(
+                status=False,
+                errorCode=-1,
+                errorMessage=f"Error while updating the conversation: {e}",
+                errorName="Error while updating the conversation",
+                errorType="ErrorWhileUpdatingConversation",
+            )
+            self.logger.error(f"{errorDTO}")
+            return errorDTO
+
+    def list_conversation_sources(self, conversation_id: int) -> ListConversationSourcesDTO:
+        """
+        Lists all data sources of the research context of a conversation.
+
+        @param conversation_id: The ID of the conversation to list data sources for.
+        @type conversation_id: int
+        @return: A DTO containing the result of the operation.
+        @rtype: ListConversationSourcesDTO
+        """
+        if conversation_id is None:
+            errorDTO = ListConversationSourcesDTO(
+                status=False,
+                errorCode=-1,
+                errorMessage="Conversation ID cannot be None",
+                errorName="Conversation ID not provided",
+                errorType="ConversationIdNotProvided",
+            )
+            self.logger.error(f"{errorDTO}")
+            return errorDTO
+
+        sqla_conversation: SQLAConversation | None = (
+            self.session.query(SQLAConversation).filter_by(id=conversation_id).first()
+        )
+
+        if sqla_conversation is None:
+            self.logger.error(f"Conversation with ID {conversation_id} not found in the database.")
+            errorDTO = ListConversationSourcesDTO(
+                status=False,
+                errorCode=-1,
+                errorMessage=f"Conversation with ID {conversation_id} not found in the database.",
+                errorName="Conversation not found",
+                errorType="ConversationNotFound",
+            )
+            self.logger.error(f"{errorDTO}")
+            return errorDTO
+
+        sqla_research_context: SQLAResearchContext | None = (
+            self.session.query(SQLAResearchContext).filter_by(id=sqla_conversation.research_context_id).first()
+        )
+
+        if sqla_research_context is None:
+            self.logger.error(
+                f"Research Context with ID {sqla_conversation.research_context_id} not found in the database."
+            )
+            errorDTO = ListConversationSourcesDTO(
+                status=False,
+                errorCode=-1,
+                errorMessage=f"Research Context with ID {sqla_conversation.research_context_id} not found in the database.",
+                errorName="Research Context not found",
+                errorType="ResearchContextNotFound",
+            )
+            self.logger.error(f"{errorDTO}")
+            return errorDTO
+
+        core_source_data: List[SourceData] = []
+
+        for sqla_source_data in sqla_research_context.source_data:
+            core_source_data.append(
+                SourceData(
+                    created_at=sqla_source_data.created_at,
+                    updated_at=sqla_source_data.updated_at,
+                    deleted=sqla_source_data.deleted,
+                    deleted_at=sqla_source_data.deleted_at,
+                    id=sqla_source_data.id,
+                    name=sqla_source_data.name,
+                    type=sqla_source_data.type,
+                    lfn=sqla_source_data.lfn,
+                    protocol=sqla_source_data.protocol,
+                )
+            )
+
+        if core_source_data == []:
+            self.logger.error(f"Research Context with ID {sqla_conversation.research_context_id} has no source data.")
+            errorDTO = ListConversationSourcesDTO(
+                status=False,
+                errorCode=-1,
+                errorMessage=f"Research Context with ID {sqla_conversation.research_context_id} has no source data.",
+                errorName="Research Context has no source data",
+                errorType="ResearchContextHasNoSourceData",
+            )
+            self.logger.error(f"{errorDTO}")
+            return errorDTO
+
+        return ListConversationSourcesDTO(
+            status=True,
+            data=core_source_data,
+        )
+
