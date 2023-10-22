74ddfab62c176da911ae540eef1f5377190ef449
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2023-10-22 16:42:33 +0200

    ConversationRepository: list_conversation_messages tested
    - Fixed problems with new fixtures getting random ranges from (1,1) (which throws an error)

--- tests/repositories/test_sqla_conversation_repository.py
+++ tests/repositories/test_sqla_conversation_repository.py
@@ -1,5 +1,10 @@
 from faker import Faker
-from lib.core.dto.conversation_repository_dto import ConversationDTO, GetConversationResearchContextDTO
+from lib.core.dto.conversation_repository_dto import (
+    ConversationDTO,
+    GetConversationResearchContextDTO,
+    ListConversationMessagesDTO,
+)
+from lib.core.entity.models import MessageBase, MessageQuery, MessageResponse, TMessageBase
 from lib.infrastructure.config.containers import Container
 from lib.infrastructure.repository.sqla.database import TDatabaseFactory
 
@@ -158,13 +163,13 @@ def test_error_get_research_context_none_conversation_id(
 ) -> None:
     conversation_repository = app_container.sqla_conversation_repository()
 
-    conv_DTO: ConversationDTO = conversation_repository.get_conversation_research_context(conversation_id=None)  # type: ignore
+    get_conv_rc_DTO: GetConversationResearchContextDTO = conversation_repository.get_conversation_research_context(conversation_id=None)  # type: ignore
 
-    assert conv_DTO.status == False
-    assert conv_DTO.errorCode == -1
-    assert conv_DTO.errorMessage == "Conversation ID cannot be None"
-    assert conv_DTO.errorName == "Conversation ID not provided"
-    assert conv_DTO.errorType == "ConversationIdNotProvided"
+    assert get_conv_rc_DTO.status == False
+    assert get_conv_rc_DTO.errorCode == -1
+    assert get_conv_rc_DTO.errorMessage == "Conversation ID cannot be None"
+    assert get_conv_rc_DTO.errorName == "Conversation ID not provided"
+    assert get_conv_rc_DTO.errorType == "ConversationIdNotProvided"
 
 
 def test_error_get_research_context_none_sqla_conversation(
@@ -173,13 +178,15 @@ def test_error_get_research_context_none_sqla_conversation(
     conversation_repository = app_container.sqla_conversation_repository()
 
     irrealistic_ID = 99999999
-    conv_DTO: ConversationDTO = conversation_repository.get_conversation_research_context(conversation_id=irrealistic_ID)  # type: ignore
+    get_conv_rc_DTO: GetConversationResearchContextDTO = conversation_repository.get_conversation_research_context(
+        conversation_id=irrealistic_ID
+    )
 
-    assert conv_DTO.status == False
-    assert conv_DTO.errorCode == -1
-    assert conv_DTO.errorMessage == f"Conversation with ID {irrealistic_ID} not found in the database."
-    assert conv_DTO.errorName == "Conversation not found"
-    assert conv_DTO.errorType == "ConversationNotFound"
+    assert get_conv_rc_DTO.status == False
+    assert get_conv_rc_DTO.errorCode == -1
+    assert get_conv_rc_DTO.errorMessage == f"Conversation with ID {irrealistic_ID} not found in the database."
+    assert get_conv_rc_DTO.errorName == "Conversation not found"
+    assert get_conv_rc_DTO.errorType == "ConversationNotFound"
 
 
 def test_error_sqla_get_research_context_(
@@ -193,3 +200,87 @@ def test_error_sqla_get_research_context_(
     TODO: not sure how to test the case where the error comes from SQLA
     """
     pass
+
+
+############
+# List Conversation Messages Feature
+############
+
+
+def test_list_conversation_messages(
+    app_container: Container,
+    db_session: TDatabaseFactory,
+    fake: Faker,
+    fake_user_with_conversation: SQLAUser,
+) -> None:
+    conversation_repository = app_container.sqla_conversation_repository()
+
+    user_with_conv = fake_user_with_conversation
+    llm = SQLALLM(
+        llm_name=fake.name(),
+        research_contexts=user_with_conv.research_contexts,
+    )
+
+    researchContext = user_with_conv.research_contexts[0]
+
+    conversation = researchContext.conversations[0]
+    conversation_title = conversation.title
+
+    messages = conversation.messages
+    message_contents = tuple([message.content for message in messages])
+
+    with db_session() as session:
+        user_with_conv.save(session=session, flush=True)
+        session.commit()
+
+    with db_session() as session:
+        result = session.query(SQLAConversation).filter_by(title=conversation_title).first()
+
+        if result is None:
+            raise Exception("Conversation not found")
+
+        list_conv_msgs_DTO: ListConversationMessagesDTO[
+            MessageBase
+        ] = conversation_repository.list_conversation_messages(conversation_id=result.id)
+
+    if list_conv_msgs_DTO.data is None:
+        raise Exception("Messages of Conversation not found")
+
+    assert list_conv_msgs_DTO.status == True
+    assert list_conv_msgs_DTO.errorCode == None
+    assert isinstance(list_conv_msgs_DTO.data, list)
+
+    for message in list_conv_msgs_DTO.data:
+        if message is None:
+            raise Exception("Message of Conversation not found")
+
+        assert message.content in message_contents
+
+
+def test_error_list_conversation_messages_none_conversation_id(
+    app_container: Container, db_session: TDatabaseFactory
+) -> None:
+    conversation_repository = app_container.sqla_conversation_repository()
+
+    list_conv_msgs_DTO: ListConversationMessagesDTO = conversation_repository.list_conversation_messages(conversation_id=None)  # type: ignore
+
+    assert list_conv_msgs_DTO.status == False
+    assert list_conv_msgs_DTO.errorCode == -1
+    assert list_conv_msgs_DTO.errorMessage == "Conversation ID cannot be None"
+    assert list_conv_msgs_DTO.errorName == "Conversation ID not provided"
+    assert list_conv_msgs_DTO.errorType == "ConversationIdNotProvided"
+
+
+def test_error_list_conversation_messages_conversation_id_not_found(
+    app_container: Container, db_session: TDatabaseFactory
+) -> None:
+    conversation_repository = app_container.sqla_conversation_repository()
+
+    irrealistic_ID = 99999999
+    list_conv_msgs_DTO: ListConversationMessagesDTO = conversation_repository.list_conversation_messages(conversation_id=irrealistic_ID)  # type: ignore
+
+    assert list_conv_msgs_DTO.status == False
+    assert list_conv_msgs_DTO.errorCode == -1
+    assert list_conv_msgs_DTO.errorMessage == f"Conversation with ID {irrealistic_ID} not found in the database."
+    assert list_conv_msgs_DTO.errorName == "Conversation not found"
+    assert list_conv_msgs_DTO.errorType == "ConversationNotFound"
