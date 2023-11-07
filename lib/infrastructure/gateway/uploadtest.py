d19afcd96c33ca08b1db0f59bbec778fecdfcd2b
piszczke <149501484+piszczke@users.noreply.github.com>
2023-11-07 01:27:06 +0100

    Update uploadtest.py
    Add of container spawn instruction and update comments

--- lib/infrastructure/gateway/uploadtest.py
+++ lib/infrastructure/gateway/uploadtest.py
@@ -1,3 +1,25 @@
+"""
+>Spawn docker container
+
+mkdir -p ~/minio/data
+docker run \
+   -p 9000:9000 \
+   -p 9090:9090 \
+   --name minio \
+   -v ~/minio/data:/data \
+   -e "MINIO_ACCESS_KEY=minio_access_key" \
+   -e "MINIO_SECRET_KEY=minio_secret_key" \
+   -e "MINIO_ROOT_USER=ROOTNAME" \
+   -e "MINIO_ROOT_PASSWORD=CHANGEME123" \
+   quay.io/minio/minio server /data --console-address ":9090"
+
+
+> to fix/investigate:
+> For some reazon command do not add keypairs of MINIO_ACCESS_KEY & MINIO_SECRET_KEY and it was added manualy from root panel on web UI 
+
+"""
+
+
 from minio import Minio
 from minio.error import S3Error
 
@@ -6,29 +28,29 @@ def main() -> None:
     # Create a client with the MinIO server playground, its access key
     # and secret key.
     client = Minio(
-        "139.19.179.6:9000",
+        "139.19.179.6:9000", #brain12 ip address
         access_key="minio_access_key",
         secret_key="minio_secret_key",
         secure=False
     )
 
-    # Make 'asiatrip' bucket if not exist.
+    # Make 'test' bucket if not exist.
     found = client.bucket_exists("testbucket")
     if not found:
         client.make_bucket("testbucket")
     else:
         print("Bucket 'testbucket' already exists")
 
-    # Upload '/home/user/Photos/asiaphotos.zip' as object name
-    # 'asiaphotos-2015.zip' to bucket 'asiatrip'.
+    # Upload path/testfile' as object name
+    # 'testfile' to bucket 'testbucket'.
     client.fput_object(
         "testbucket",
         "testfile",
         "testfile",
     )
     print(
-        "'/home/user/Photos/asiaphotos.zip' is successfully uploaded as "
-        "object 'asiaphotos-2015.zip' to bucket 'asiatrip'."
+        "'path/testfile' is successfully uploaded as "
+        "object 'testfile' to bucket 'testbucket'."
     )
 
 
