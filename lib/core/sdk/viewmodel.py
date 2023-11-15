a7d9293c793f070f16fcf87bacac31050e64b2bb
Mayank Sharma <imptodefeat@gmail.com>
2023-11-15 03:51:48 +0100

    add base view model validation

--- lib/core/sdk/viewmodel.py
+++ lib/core/sdk/viewmodel.py
@@ -1,22 +1,42 @@
-from typing import Union, TypeVar
-from pydantic import BaseModel
+from typing import Any, TypeVar
+from pydantic import BaseModel, model_validator, root_validator, validator
 
 
-class BaseSuccessViewModel(BaseModel):
-    status: bool = True
+class BaseViewModel(BaseModel):
+    status: bool
+    code: int
+    errorCode: int | None = None
+    errorMessage: str | None = None
+    errorName: str | None = None
+    errorType: str | None = None
+
+    @model_validator(mode="after")  # type: ignore
+    def error_fields_absent_in_successful_response(cls, values: "BaseViewModel") -> "BaseViewModel":
+        v = values.status
+        if v:
+            if values.errorCode:
+                raise ValueError("errorCode should not be present in a successful response")
+            if values.errorMessage:
+                raise ValueError("errorMessage should not be present in a successful response")
+            if values.errorName:
+                raise ValueError("errorName should not be present in a successful response")
+            if values.errorType:
+                raise ValueError("errorType should not be present in a successful response")
+        else:
+            if not values.errorCode:
+                raise ValueError("errorCode should be present in an unsuccessful response")
+            if not values.errorMessage:
+                raise ValueError("errorMessage should be present in an unsuccessful response")
+            if not values.errorName:
+                raise ValueError("errorName should be present in an unsuccessful response")
+            if not values.errorType:
+                raise ValueError("errorType should be present in an unsuccessful response")
+        if not v:
+            if values.code != values.errorCode:
+                raise ValueError("code should be equal to errorCode in an unsuccessful response")
+            if values.code >= 200 and values.code < 300:
+                raise ValueError("code should NOT be between 200 and 299 in an unsuccessful response")
+        return values
 
 
-TBaseSuccessViewModel = TypeVar("TBaseSuccessViewModel", bound=BaseSuccessViewModel, covariant=True)
-
-
-class BaseErrorViewModel(BaseModel):
-    status: bool = False
-    errorCode: int
-    errorMessage: str
-    errorName: str
-    errorType: str
-
-
-TBaseErrorViewModel = TypeVar("TBaseErrorViewModel", bound=BaseErrorViewModel, covariant=True)
-
-TBaseViewModel = Union[TBaseSuccessViewModel, TBaseErrorViewModel]
+TBaseViewModel = TypeVar("TBaseViewModel", bound=BaseViewModel, covariant=True)
