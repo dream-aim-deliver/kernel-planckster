c0b35b57ed633f7b26e6cd0d6bac398a2662e771
Mayank Sharma <imptodefeat@gmail.com>
2023-11-16 02:18:00 +0100

    api: test controller, add to demo container #37

--- tests/api/test_demo.py
+++ tests/api/test_demo.py
@@ -1,6 +1,7 @@
 from lib.core.usecase_models.demo_usecase_models import DemoRequest, DemoResponse
 from lib.core.view_model.demo_view_model import DemoViewModel
 from lib.infrastructure.config.containers import Container
+from lib.infrastructure.controller.demo_controller import DemoControllerParameters
 
 
 def test_demo_presenter(app_container: Container) -> None:
@@ -18,3 +19,18 @@ def test_demo_usecase(app_container: Container) -> None:
     response = usecase.execute(request=DemoRequest(numbers=[1, 2, 3]))
     assert response is not None
     assert response.sum == 6
+
+
+def test_demo_controller(app_container: Container) -> None:
+    controller = app_container.demo.controller()
+    assert controller is not None
+    view_model = controller.execute(parameters=None)
+    assert view_model is not None
+    assert view_model.status is True
+    assert view_model.sum == 0
+
+    controller_parameters: DemoControllerParameters = DemoControllerParameters(num1=1, num2=2)
+    view_model = controller.execute(parameters=controller_parameters)
+    assert view_model is not None
+    assert view_model.status is True
+    assert view_model.sum == 3
