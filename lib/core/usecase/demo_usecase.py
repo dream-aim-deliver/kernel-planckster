24dfc4392f67d026af42022e54fc56f2772132a9
Mayank Sharma <imptodefeat@gmail.com>
2023-11-16 02:05:31 +0100

    api: refactor presenter to use specific types instead of generics #37

--- lib/core/usecase/demo_usecase.py
+++ lib/core/usecase/demo_usecase.py
@@ -1,8 +1,9 @@
+from lib.core.ports.primary.demo_ports import DemoInputPort
 from lib.core.sdk.usecase import BaseUseCase
 from lib.core.sdk.usecase_models import BaseErrorResponse
 from lib.core.usecase_models.demo_usecase_models import DemoRequest, DemoResponse
 
 
-class DemoUseCase(BaseUseCase[DemoRequest, DemoResponse, BaseErrorResponse]):
+class DemoUseCase(DemoInputPort):
     def execute(self, request: DemoRequest) -> DemoResponse | BaseErrorResponse:
         return DemoResponse(sum=sum(request.numbers))
