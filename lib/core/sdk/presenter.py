a3a1e958156f62ef2c822a0a0145125b46af5f58
Mayank Sharma <imptodefeat@gmail.com>
2023-11-16 01:50:36 +0100

    api: handle view model validation errors #37

--- lib/core/sdk/presenter.py
+++ lib/core/sdk/presenter.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
 from typing import Generic, Protocol, runtime_checkable
+
+from pydantic import ValidationError
 from lib.core.sdk.primary_ports import BaseOutputPort
 
 from lib.core.sdk.usecase_models import (
+    BaseErrorResponse,
     TBaseErrorResponse,
     TBaseResponse,
 )
@@ -17,7 +20,18 @@ class BasePresenter(
     Generic[TBaseResponse, TBaseErrorResponse, TBaseViewModel],
 ):
     def present_success(self, response: TBaseResponse) -> TBaseViewModel:
-        return self.convert_response_to_view_model(response)
+        try:
+            view_model = self.convert_response_to_view_model(response)
+            return view_model
+        except ValidationError as error:
+            return self.convert_error_response_to_view_model(
+                BaseErrorResponse(  # type: ignore
+                    errorCode=500,
+                    errorName="ValidationError",
+                    errorMessage="ValidationError",
+                    errorType="ValidationError",
+                )
+            )
 
     def present_error(self, response: TBaseErrorResponse) -> TBaseViewModel:
         return self.convert_error_response_to_view_model(response)
