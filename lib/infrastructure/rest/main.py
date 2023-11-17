445b43c1d9c313234c28d00aeff0eb0516007107
Mayank Sharma <imptodefeat@gmail.com>
2023-11-17 02:55:49 +0100

    dynamically load features from planckster containers to fastapi #37

--- lib/infrastructure/rest/main.py
+++ lib/infrastructure/rest/main.py
@@ -1,11 +1,15 @@
+import importlib
+from pathlib import Path
 from typing import Any
 from fastapi import APIRouter, FastAPI
 import subprocess
 
 import uvicorn
+from lib.core.sdk.fastapi import FastAPIFeature
 from lib.core.sdk.utils import get_all_modules
 from lib.infrastructure.config.containers import ApplicationContainer
-from lib.infrastructure.rest.endpoints.demo_feature_fastapi_endpoints import DemoFastAPIFeature
+import lib.infrastructure.rest.endpoints as endpoints
+
 
 app_container = ApplicationContainer()
 
@@ -16,11 +20,26 @@ def create_app() -> tuple[FastAPI, ApplicationContainer]:
     app_container.config.from_yaml("../../../config.yaml")
     app.container = app_container  # type: ignore
 
-    # all_features = get_all_modules()
-    demo_feature = DemoFastAPIFeature()
-    router: APIRouter | None = demo_feature.load()
-    if router is not None:
-        app.include_router(demo_feature.router)
+    fastapi_endpoints = get_all_modules(package=endpoints, relative_package_dir=Path(__file__).parent / "endpoints")
+
+    for fastapi_endpoint in fastapi_endpoints:
+        module = importlib.import_module(fastapi_endpoint)
+
+        fastapi_feature_class = next(
+            (
+                obj
+                for name, obj in module.__dict__.items()
+                if isinstance(obj, type) and "FastAPIFeature" in obj.__name__ and obj != FastAPIFeature
+            ),
+            None,
+        )
+        if fastapi_feature_class is None:
+            continue
+        else:
+            fastapi_feature = fastapi_feature_class()
+            router: APIRouter | None = fastapi_feature.load()
+            if router is not None:
+                app.include_router(fastapi_feature.router)
     return app, app_container
 
 
