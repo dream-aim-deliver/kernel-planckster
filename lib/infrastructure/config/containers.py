cfd212863efc43aad099821bdd9ec92764900bc5
Mayank Sharma <imptodefeat@gmail.com>
2023-11-17 02:19:32 +0100

    dynamic wirng of fastapi endpoints #37

--- lib/infrastructure/config/containers.py
+++ lib/infrastructure/config/containers.py
@@ -1,11 +1,16 @@
+import importlib
+import os
 import sys
 import logging.config
+from types import ModuleType
+from typing import List
 from dependency_injector import containers, providers
 from lib.infrastructure.config.demo_feature_container import DemoFeatureContainer
-
+import lib.infrastructure.rest.endpoints as endpoints
 
 from lib.infrastructure.repository.sqla.database import Database
 from lib.infrastructure.repository.sqla.sqla_conversation_repository import SQLAConversationRepository
+from pathlib import Path
 
 
 class FeatureContainer(containers.DeclarativeContainer):
@@ -13,8 +18,26 @@ class FeatureContainer(containers.DeclarativeContainer):
     demo = providers.Container(DemoFeatureContainer, config=config.demo)
 
 
+def get_all_modules(package: ModuleType) -> List[str]:
+    package_dir = str(Path(__file__).parent.parent / "rest" / "endpoints")
+    modules = []
+    for filename in os.listdir(package_dir):
+        if filename.endswith(".py") and not filename.startswith("__"):
+            module_name = filename[:-3]  # Remove the '.py' extension
+            full_module_name = f"{package.__name__}.{module_name}"
+            # importlib.import_module(full_module_name)
+            modules.append(full_module_name)
+    return modules
+
+
 class ApplicationContainer(containers.DeclarativeContainer):
     config = providers.Configuration(yaml_files=["./config.yaml"])
+
+    # Dynamic wiring of fastapi endpoints:
+    modules = get_all_modules(endpoints)
+    wiring_config = containers.WiringConfiguration(
+        modules=modules,
+    )
 
     logging = providers.Resource(
         logging.basicConfig,
@@ -32,9 +55,8 @@ class ApplicationContainer(containers.DeclarativeContainer):
         db_name=config.rdbms.database,
     )
 
-    # Features and Feature Modules:
+    # Features:
     features = providers.Container(FeatureContainer, config=config.features)
-    wiring_config = containers.WiringConfiguration(modules=[".demo_feature"])
 
     # Repositories:
     sqla_conversation_repository: providers.Factory[SQLAConversationRepository] = providers.Factory(
