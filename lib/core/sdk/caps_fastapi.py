4d075d812293d858e805c1fda0c6da741f23dc18
Mayank Sharma <imptodefeat@gmail.com>
2023-11-17 01:14:43 +0100

    refactor FastAPIFeature #37

--- lib/core/sdk/caps_fastapi.py
+++ lib/core/sdk/caps_fastapi.py
@@ -1,83 +1,104 @@
-from abc import abstractmethod
-from typing import Annotated, Any, Dict, Generic, Literal, Type, TypeVar
-from fastapi import APIRouter, Depends, HTTPException, Request, Response
-from pydantic import BaseModel, ConfigDict, Field, model_validator, validator
-from lib.core.sdk.controller import BaseControllerParameters, TBaseControllerParameters
+from abc import ABC, abstractmethod
+from enum import Enum
+from typing import Annotated, Any, Dict, Generic, TypeVar
+from fastapi import APIRouter, HTTPException, Request, Response
+from pydantic import BaseModel
+from lib.core.sdk.controller import BaseController, TBaseControllerParameters
 from lib.core.sdk.feature import BaseFeatureDescriptor
 
-from lib.core.sdk.usecase_models import (
-    BaseErrorResponse,
-    BaseRequest,
-    BaseResponse,
-    TBaseErrorResponse,
-    TBaseRequest,
-    TBaseResponse,
-)
 from lib.core.sdk.viewmodel import (
-    BaseViewModel,
-    TBaseViewModel,
     TBaseViewModel,
 )
 
-T = TypeVar("T", bound=BaseViewModel)
+TQueryParameters = TypeVar("TQueryParameters", bound=BaseModel)
+TBodyParameters = TypeVar("TBodyParameters", bound=BaseModel)
 
 
-class FastAPIViewModelWrapper(BaseModel, Generic[T]):
-    data: T | None = None
+class FastAPIFeature(ABC, Generic[TQueryParameters, TBodyParameters, TBaseControllerParameters, TBaseViewModel]):
+    def __init__(
+        self,
+        name: str,
+        controller: BaseController[TBaseControllerParameters, Any, Any, Any, TBaseViewModel],
+        descriptor: BaseFeatureDescriptor,
+        responses: Dict[int | str, dict[str, Any]],
+    ) -> None:
+        self._name = name
+        self._controller = controller
+        self._descriptor = descriptor
+        self._responses: Dict[int | str, dict[str, Any]] = responses
 
+        tags: list[str | Enum] = [name]
+        if self._descriptor.auth:
+            tags.append("Protectected Endpoints")
+        else:
+            tags.append("Public Endpoints")
 
-class FastAPIFeature:
-    group: str
-    responses: Dict[int | str, dict[str, Any]] | None = None
-    auth_required: bool = False
-    router: APIRouter | None = None
+        self._router: APIRouter = APIRouter(
+            prefix=f"/{name.lower()}",
+            tags=tags,
+        )
 
-    model_config = ConfigDict(
-        arbitrary_types_allowed=True,
-    )
+    @property
+    def name(self) -> str:
+        return self._name
 
-    # @model_validator(mode="after")  # type: ignore
-    # def populate_arbitrary_fields(
-    #     cls,
-    #     instance: "FastAPIFeature[BaseControllerParameters, BaseRequest, BaseResponse, BaseErrorResponse, BaseViewModel]",
-    # ) -> "FastAPIFeature[BaseControllerParameters, BaseRequest, BaseResponse, BaseErrorResponse, BaseViewModel]":
-    #     group = instance.group
-    #     instance.router = APIRouter(prefix=f"/{group}", tags=[group])
-    #     instance.register_endpoints(instance.router)
+    @property
+    def controller(
+        self,
+    ) -> BaseController[TBaseControllerParameters, Any, Any, Any, TBaseViewModel]:
+        return self._controller
+
+    @property
+    def descriptor(self) -> BaseFeatureDescriptor:
+        return self._descriptor
+
+    @property
+    def responses(self) -> Dict[int | str, dict[str, Any]]:
+        return self._responses
+
+    @property
+    def router(self) -> APIRouter:
+        return self._router
+
+    def register_routes(self) -> None:
+        self.router.add_api_route(
+            name=self.descriptor.name,
+            description=self.descriptor.description,
+            methods=[self.descriptor.verb],
+            path=self.descriptor.endpoint,
+            endpoint=self.endpoint_fn,
+            responses=self.responses,
+        )
+
+    def load(self) -> APIRouter | None:
+        if self.descriptor.enabled:
+            self.register_routes()
+            return self.router
+        else:
+            return None
 
     @abstractmethod
+    def create_controller_parameters(
+        self, query: TQueryParameters | None, body: TBodyParameters | None
+    ) -> TBaseControllerParameters:
+        raise NotImplementedError("You must implement the create_controller_parameters method in your feature")
+
     def endpoint_fn(
         self,
         request: Request,
         response: Response,
-        request_query_parameters: TBaseControllerParameters | None = None,
-        request_body_parameters: TBaseRequest | None = None,
+        request_query_parameters: TQueryParameters | None = None,
+        request_body_parameters: TBodyParameters | None = None,
     ) -> TBaseViewModel:
-        raise NotImplementedError("You must implement the endpoint_fn method in your feature")
-
-    def handle_request(
-        self,
-        controller_parameters: TBaseControllerParameters | None = None,
-    ) -> TBaseViewModel:
-        controller = self.controller_factory()
-        view_model = controller.execute(controller_parameters)
-        if view_model is None:
-            raise HTTPException(
-                status_code=500,
-                detail=f"Something went wrong. Controller for {self.verb} {self.endpoint} of feature {self.name} did not produce a view model.",
-            )
-        if not view_model.status:
-            raise HTTPException(status_code=500, detail=view_model)
-        else:
-            return view_model
-
-    def register_endpoints(self, router: APIRouter) -> None:
-        router.add_api_route(
-            methods=[self.verb],
-            tags=[self.group],
-            path=f"{self.endpoint}",
-            name=self.name,
-            description=self.description,
-            endpoint=self.endpoint_fn,
-            responses=self.responses,
+        controller_parameters: TBaseControllerParameters = self.create_controller_parameters(
+            query=request_query_parameters,
+            body=request_body_parameters,
         )
+        view_model: TBaseViewModel | None = self.controller.execute(
+            parameters=controller_parameters,
+        )
+        if view_model is None:
+            raise HTTPException(500, "View model is None")
+        else:
+            response.status_code = view_model.code
+            return view_model
--- lib/infrastructure/config/demo_feature.py
+++ lib/infrastructure/config/demo_feature.py
@@ -2,6 +2,7 @@ from enum import Enum
 from typing import Annotated, Any, Dict
 
 from fastapi import APIRouter, Depends, Request, Response
+from lib.core.sdk.caps_fastapi import FastAPIFeature
 
 from lib.core.sdk.feature import BaseFeatureDescriptor
 from lib.core.view_model.demo_view_model import DemoViewModel
@@ -11,79 +12,52 @@ from lib.infrastructure.config.containers import ApplicationContainer
 from dependency_injector.wiring import inject, Provide
 
 
-class DemoFastAPIFeature:
+class DemoFastAPIQueryParameters(DemoControllerParameters):
+    pass
+
+
+class DemoFastAPIFeature(FastAPIFeature[DemoFastAPIQueryParameters, Any, DemoControllerParameters, DemoViewModel]):
     @inject
     def __init__(
         self,
         descriptor_provider: Any = Provide[ApplicationContainer.features.demo.feature_descriptor.provider],  # type: ignore
         controller_provider: Any = Provide[ApplicationContainer.features.demo.controller.provider],  # type: ignore
     ) -> None:
-        self._descriptor: BaseFeatureDescriptor = descriptor_provider()
-        tags: list[str | Enum] = [self._descriptor.name]
-        if self._descriptor.auth:
-            tags.append("auth_required")
-        # TODO - if auth is required, then add a dependency to token validator fn in the router description below. See https://fastapi.tiangolo.com/tutorial/dependencies/global-dependencies/?
-        self._router: APIRouter = APIRouter(
-            prefix="/demo",
-            tags=tags,
-        )
-        self._controller: DemoController = controller_provider()
-        self._responses: Dict[int | str, dict[str, Any]] = {
-            200: {
-                "model": DemoViewModel,
-                "description": "Success",
+        descriptor: BaseFeatureDescriptor = descriptor_provider()
+        controller: DemoController = controller_provider()
+        super().__init__(
+            name="Demo",
+            controller=controller,
+            descriptor=descriptor,
+            responses={
+                200: {
+                    "model": DemoViewModel,
+                    "description": "Success",
+                },
+                500: {
+                    "model": DemoViewModel,
+                    "description": "Internal Server Error",
+                },
             },
-            500: {
-                "model": DemoViewModel,
-                "description": "Internal Server Error",
-            },
-        }
-
-    @property
-    def descriptor(self) -> BaseFeatureDescriptor:
-        return self._descriptor
-
-    @property
-    def responses(self) -> Dict[int | str, dict[str, Any]]:
-        return self._responses
-
-    @property
-    def router(self) -> APIRouter:
-        return self._router
-
-    @property
-    def controller(self) -> DemoController:
-        return self._controller
-
-    def routes(self) -> None:
-        self.router.add_api_route(
-            name=self.descriptor.name,
-            description=self.descriptor.description,
-            methods=[self.descriptor.verb],
-            path=self.descriptor.endpoint,
-            endpoint=self.endpoint_fn,
-            responses=self.responses,
         )
 
-    def load(self) -> APIRouter | None:
-        if self.descriptor.enabled:
-            self.routes()
-            return self.router
+    def create_controller_parameters(
+        self, query: DemoFastAPIQueryParameters | None, body: Any
+    ) -> DemoControllerParameters:
+        if query is None:
+            return DemoControllerParameters(num1=0, num2=0)
         else:
-            return None
+            return DemoControllerParameters(num1=query.num1, num2=query.num2)
 
-    def endpoint_fn(
+    def endpoint_fn(  # type: ignore
         self,
         request: Request,
         response: Response,
-        request_query_parameters: Annotated[DemoControllerParameters, Depends()],
-        # request_body_parameters: DemoControllerParameters,
+        request_query_parameters: Annotated[DemoFastAPIQueryParameters, Depends()],
     ) -> DemoViewModel | None:
-        # Make controller parameters here with your FastAPI request parameters
-        controllerParameters: DemoControllerParameters = request_query_parameters
-        view_model: DemoViewModel | None = self.controller.execute(controllerParameters)
-        if view_model is None:
-            return None
-        else:
-            response.status_code = view_model.code
-            return view_model
+        view_model: DemoViewModel | None = super().endpoint_fn(
+            request=request,
+            response=response,
+            request_query_parameters=request_query_parameters,
+        )
+        return view_model
