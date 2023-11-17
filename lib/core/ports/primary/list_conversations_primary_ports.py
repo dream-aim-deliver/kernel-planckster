b26610183b64055502ce2e9c8da0c21f48dcbf33
Mayank Sharma <imptodefeat@gmail.com>
2023-11-17 19:13:47 +0100

    feature: list_conversations,  error response model #59

--- lib/core/ports/primary/list_conversations_primary_ports.py
+++ lib/core/ports/primary/list_conversations_primary_ports.py
@@ -3,23 +3,26 @@ from lib.core.sdk.primary_ports import BaseOutputPort
 from lib.core.sdk.usecase import BaseUseCase
 from lib.core.sdk.usecase_models import BaseErrorResponse
 from lib.core.usecase_models.list_conversations_usecase_models import (
+    ListConversationsError,
     ListConversationsRequest,
     ListConversationsResponse,
 )
 from lib.core.view_model.list_conversations_view_model import ListConversationsViewModel
 
 
-class ListConversationsInputPort(BaseUseCase[ListConversationsRequest, ListConversationsResponse, BaseErrorResponse]):
+class ListConversationsInputPort(
+    BaseUseCase[ListConversationsRequest, ListConversationsResponse, ListConversationsError]
+):
     @abstractmethod
-    def execute(self, request: ListConversationsRequest) -> ListConversationsResponse | BaseErrorResponse:
+    def execute(self, request: ListConversationsRequest) -> ListConversationsResponse | ListConversationsError:
         raise NotImplementedError("This method must be implemented by the usecase.")
 
 
 class ListConversationsOutputPort(
-    BaseOutputPort[ListConversationsResponse, BaseErrorResponse, ListConversationsViewModel]
+    BaseOutputPort[ListConversationsResponse, ListConversationsError, ListConversationsViewModel]
 ):
     @abstractmethod
-    def convert_error_response_to_view_model(self, response: BaseErrorResponse) -> ListConversationsViewModel:
+    def convert_error_response_to_view_model(self, response: ListConversationsError) -> ListConversationsViewModel:
         raise NotImplementedError(
             "You must implement the convert_error_response_to_view_model method in your presenter"
         )
