599f6fed1423492d616604b5f5a3d8681a61957f
Mayank Sharma <imptodefeat@gmail.com>
2023-11-17 19:13:47 +0100

    feature: list_conversations,  error response model #59

--- lib/core/usecase_models/list_conversations_usecase_models.py
+++ lib/core/usecase_models/list_conversations_usecase_models.py
@@ -1,7 +1,7 @@
 from typing import List
 from pydantic import Field
 from lib.core.entity.models import Conversation
-from lib.core.sdk.usecase_models import BaseRequest, BaseResponse
+from lib.core.sdk.usecase_models import BaseErrorResponse, BaseRequest, BaseResponse
 
 
 class ListConversationsRequest(BaseRequest):
@@ -17,4 +17,13 @@ class ListConversationsResponse(BaseResponse):
     Response Model for the List Conversations Use Case.
     """
 
-    data: List[Conversation] = Field(description="List of conversations in the research context.")
+    research_context_id: int = Field(description="Research context id for which the conversations are to be listed.")
+    conversations: List[Conversation] = Field(description="List of conversations in the research context.")
+
+
+class ListConversationsError(BaseErrorResponse):
+    """
+    Error Response Model for the List Conversations Use Case.
+    """
+
+    research_context_id: int = Field(description="Research context id that was passed in the request.")
