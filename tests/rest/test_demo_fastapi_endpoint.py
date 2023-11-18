deda1125b2a212d9e7228e2e60ebe97982d01892
Mayank Sharma <imptodefeat@gmail.com>
2023-11-18 04:53:54 +0100

    feature: list_conversations, usecase #59

--- tests/rest/test_demo_fastapi_endpoint.py
+++ tests/rest/test_demo_fastapi_endpoint.py
@@ -1,10 +1,13 @@
 from fastapi.testclient import TestClient
 
 from lib.core.view_model.demo_view_model import DemoViewModel
+from lib.infrastructure.config.containers import ApplicationContainer
 
 
-def test_demo_fastapi_get_endpoint_returns_valid_response(client: TestClient) -> None:
-    response = client.get("/demo/sum", params={"num1": 1, "num2": 2})
+def test_demo_fastapi_get_endpoint_returns_valid_response(
+    client: TestClient, app_container: ApplicationContainer
+) -> None:
+    response = client.get("/sum", params={"num1": 1, "num2": 2})
     assert response.status_code == 200
     data = response.json()
     assert (
@@ -17,6 +20,6 @@ def test_demo_fastapi_get_endpoint_returns_valid_response(client: TestClient) ->
     )
 
 
-def test_demo_fastapi_get_endpoint_invalid_query(client: TestClient) -> None:
-    response = client.get("/demo/sum")
+def test_demo_fastapi_get_endpoint_invalid_query(client: TestClient, app_container: ApplicationContainer) -> None:
+    response = client.get("/sum")
     assert response.status_code == 422
