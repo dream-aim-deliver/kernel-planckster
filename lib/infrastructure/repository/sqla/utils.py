11c191a1962f42da143ef5be009b92f21b1fb69e
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2023-11-18 02:39:24 +0100

    ConversationRepository: added utils to convert from SQLA to core models #50
    - Improved new_conversation code with one less call to the DB

--- lib/infrastructure/repository/sqla/utils.py
+++ lib/infrastructure/repository/sqla/utils.py
@@ -1,5 +1,11 @@
-from lib.core.entity.models import Conversation, ResearchContext
-from lib.infrastructure.repository.sqla.models import SQLAConversation, SQLAResearchContext
+from lib.core.entity.models import Conversation, MessageQuery, MessageResponse, ResearchContext, SourceData
+from lib.infrastructure.repository.sqla.models import (
+    SQLAConversation,
+    SQLAMessageQuery,
+    SQLAMessageResponse,
+    SQLAResearchContext,
+    SQLASourceData,
+)
 
 
 def convert_sqla_research_context_to_core_research_context(
@@ -39,4 +45,68 @@ def convert_sqla_conversation_to_core_conversation(sqla_conversation: SQLAConver
         deleted_at=sqla_conversation.deleted_at,
         id=sqla_conversation.id,
         title=sqla_conversation.title,
+    )
+
+
+def convert_sqla_message_query_to_core_message_query(sqla_message_query: SQLAMessageQuery) -> MessageQuery:
+    """
+    Converts a SQLAMessageQuery to a (core) MessageQuery
+
+    @param sqla_message_query: The SQLAMessageQuery to convert
+    @type sqla_message_query: SQLAMessageQuery
+    @return: The converted MessageQuery
+    @rtype: MessageQuery
+    """
+    return MessageQuery(
+        created_at=sqla_message_query.created_at,
+        updated_at=sqla_message_query.updated_at,
+        deleted=sqla_message_query.deleted,
+        deleted_at=sqla_message_query.deleted_at,
+        id=sqla_message_query.id,
+        content=sqla_message_query.content,
+        timestamp=sqla_message_query.timestamp,
+    )
+
+
+def convert_sqla_message_response_to_core_message_response(
+    sqla_message_response: SQLAMessageResponse,
+) -> MessageResponse:
+    """
+    Converts a SQLAMessageResponse to a (core) MessageResponse
+
+    @param sqla_message_response: The SQLAMessageResponse to convert
+    @type sqla_message_response: SQLAMessageResponse
+    @return: The converted MessageResponse
+    @rtype: MessageResponse
+    """
+    return MessageResponse(
+        created_at=sqla_message_response.created_at,
+        updated_at=sqla_message_response.updated_at,
+        deleted=sqla_message_response.deleted,
+        deleted_at=sqla_message_response.deleted_at,
+        id=sqla_message_response.id,
+        content=sqla_message_response.content,
+        timestamp=sqla_message_response.timestamp,
+    )
+
+
+def convert_sqla_source_data_to_core_source_data(sqla_source_data: SQLASourceData) -> SourceData:
+    """
+    Converts a SQLASourceData to a (core) SourceData
+
+    @param sqla_source_data: The SQLASourceData to convert
+    @type sqla_source_data: SQLASourceData
+    @return: The converted SourceData
+    @rtype: SourceData
+    """
+    return SourceData(
+        created_at=sqla_source_data.created_at,
+        updated_at=sqla_source_data.updated_at,
+        deleted=sqla_source_data.deleted,
+        deleted_at=sqla_source_data.deleted_at,
+        id=sqla_source_data.id,
+        name=sqla_source_data.name,
+        type=sqla_source_data.type,
+        lfn=sqla_source_data.lfn,
+        protocol=sqla_source_data.protocol,
     )
