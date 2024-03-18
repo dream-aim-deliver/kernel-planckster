44967e7fb8c0f8a0852cdbed874c467d76dd0cb8
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2024-03-18 13:35:22 +0100

    File Repository: independized file_path_to_lfn function
    - Now accessible from core, so it can be called by use cases
    - Reimplemented the minio_file_repository to adapt to the changes
    - Updated tests

--- lib/core/dto/file_repository_dto.py
+++ lib/core/dto/file_repository_dto.py
@@ -2,9 +2,20 @@ from lib.core.entity.models import LFN
 from lib.core.sdk.dto import BaseDTO
 
 
+class FilePathToLFNDTO(BaseDTO):  # type: ignore  # TODO: decide how to model these data entering core
+    """
+    A DTO for whenever a file path is converted to an LFN.
+
+    @param lfn: The logical file name of the file.
+    @type lfn: LFN
+    """
+
+    lfn: LFN | None = None
+
+
 class UploadFileDTO(BaseDTO):  # type: ignore  # TODO: decide how to model these data entering core
     """
-    A DTO for whenever source data is uploaded.
+    A DTO for whenever source data is uploaded by the user.
 
     @param lfn: The logical file name of the file that was uploaded.
     @type lfn: LFN
--- lib/core/ports/secondary/file_repository.py
+++ lib/core/ports/secondary/file_repository.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
 import logging
 
-from lib.core.dto.file_repository_dto import UploadFileDTO
+from lib.core.dto.file_repository_dto import FilePathToLFNDTO, UploadFileDTO
+from lib.core.entity.models import LFN
 
 
 class FileRepositoryOutputPort(ABC):
@@ -20,12 +21,24 @@ class FileRepositoryOutputPort(ABC):
         return self._logger
 
     @abstractmethod
-    def upload_file(self, file_path: str) -> UploadFileDTO:
+    def file_path_to_lfn(self, file_path: str) -> FilePathToLFNDTO:
+        """
+        Converts a local file path to a logical file name.
+
+        @param file_path: The path to the file.
+        @type file_path: str
+        @return: A DTO containing the result of the operation.
+        @rtype: FilePathToLFNDTO
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def upload_file(self, lfn: LFN) -> UploadFileDTO:
         """
         Uploads source data.
 
-        @param file_path: The name of the file to upload.
-        @type file_path: str
+        @param lfn: The logical file name of the file to upload.
+        @type lfn: LFN
         @return: A DTO containing the result of the operation.
         @rtype: UploadSourceDataDTO
         """
