f87ed29e5074649a8b2abd0579d8301f17eb7fdb
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2024-03-18 16:13:08 +0100

    UploadFile Feature: implemented usecase, controller, presenter, and wired them
    - Tested that they work with an actual test file

---
+++ lib/core/usecase/upload_file_usecase.py
@@ -0,0 +1,40 @@
+from lib.core.dto.file_repository_dto import FilePathToLFNDTO, UploadFileDTO
+from lib.core.ports.primary.upload_file_primary_ports import UploadFileInputPort
+from lib.core.usecase_models.upload_file_usecase_models import UploadFileError, UploadFileRequest, UploadFileResponse
+
+
+class UploadFileUsecase(UploadFileInputPort):
+    def execute(self, request: UploadFileRequest) -> UploadFileResponse | UploadFileError:
+        file_path = request.file_path
+
+        lfn_dto: FilePathToLFNDTO = self.file_repository.file_path_to_lfn(file_path=file_path)
+
+        if not lfn_dto.status:
+            return UploadFileError(
+                errorCode=lfn_dto.errorCode,
+                errorMessage=lfn_dto.errorMessage,
+                errorName=lfn_dto.errorName,
+                errorType=lfn_dto.errorType,
+            )
+
+        lfn = lfn_dto.lfn
+
+        if not lfn:
+            return UploadFileError(
+                errorCode="LFN_NOT_FOUND",
+                errorMessage="LFN not found",
+                errorName="LFN_NOT_FOUND",
+                errorType="LFN_NOT_FOUND",
+            )
+
+        dto: UploadFileDTO = self.file_repository.upload_file(lfn=lfn)
+
+        if dto.status:
+            return UploadFileResponse(lfn=dto.lfn, auth=dto.auth)
+
+        return UploadFileError(
+            errorCode=dto.errorCode,
+            errorMessage=dto.errorMessage,
+            errorName=dto.errorName,
+            errorType=dto.errorType,
+        )
