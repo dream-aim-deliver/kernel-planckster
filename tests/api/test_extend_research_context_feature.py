124dd5ccb11bd84c7c0efa1e8c116fff5ce207db
Yotam <paxarchy@gmail.com>
2024-07-01 18:25:25 -0400

    Extend Research Context Feature: test and error improvements

--- tests/api/test_extend_research_context_feature.py
+++ tests/api/test_extend_research_context_feature.py
@@ -31,20 +31,22 @@ def test_extend_research_context_usecase(
     app_initialization_container: ApplicationContainer,
     db_session: TDatabaseFactory,
     fake: Faker,
-    fake_client_with_conversation: SQLAClient,  # Fake client with Research Context
+    fake_client_with_research_context_and_new_sources: SQLAClient,  # Fake client with Research Context
     fake_source_data_list: list[SQLAResearchContext],
 ) -> None:
     usecase: ExtendResearchContextUseCase = app_initialization_container.extend_research_context_feature.usecase()
 
     assert usecase is not None
 
-    client_with_context = fake_client_with_conversation
+    client_with_context = fake_client_with_research_context_and_new_sources
     llm_name = fake.name()
 
-    # Make sure all source data have valid, unique IDs
+    existing_research_context = random.choice(client_with_context.research_contexts)
+    # Populate source data into the existing research context
     existing_source_data_ids = []
-    for context in client_with_context.research_contexts:
-        existing_source_data_ids.extend([sd.id for sd in context.source_data])
+    for source_datum in client_with_context.source_data:
+        existing_research_context.source_data.append(source_datum)
+        existing_source_data_ids.append(source_datum.id)
 
     incoming_source_data_list = fake_source_data_list
     new_source_data_list: List[SQLASourceData] = []
@@ -52,16 +54,17 @@ def test_extend_research_context_usecase(
         while source_data.id is None or source_data.id in existing_source_data_ids + new_source_data_list:
             source_data.id = random.randint(1001, 2000)
         new_source_data_list.append(source_data)
+
     client_with_context.source_data.extend(new_source_data_list)
     new_source_data_ids = [sd.id for sd in new_source_data_list]
+    assert new_source_data_ids != []
 
     llm = SQLALLM(
         llm_name=llm_name,
         research_contexts=client_with_context.research_contexts,
     )
 
-    existing_research_context = random.choice(client_with_context.research_contexts)
-    # Make titles unique to query later
+    # Make title unique to query later
     existing_research_context_title = f"{existing_research_context.title}-{uuid.uuid4()}"
     existing_research_context.title = existing_research_context_title
 
@@ -75,6 +78,9 @@ def test_extend_research_context_usecase(
         )
 
         assert queried_existing_research_context is not None
+        queried_existing_source_data_list = queried_existing_research_context.source_data
+        queried_existing_source_data_ids = [sd.id for sd in queried_existing_source_data_list]
+        assert queried_existing_source_data_ids != new_source_data_ids
 
         queried_client = session.get(SQLAClient, queried_existing_research_context.client_id)
 
@@ -102,17 +108,19 @@ def test_extend_research_context_usecase(
         queried_new_research_context = session.get(SQLAResearchContext, response.research_context.id)
 
         assert queried_new_research_context is not None
+        assert queried_new_research_context.title == new_research_context_title
+        assert queried_new_research_context.description == new_research_context_description
 
         queried_new_research_context_source_data_list = queried_new_research_context.source_data
         queried_new_research_context_source_data_ids = [sd.id for sd in queried_new_research_context_source_data_list]
 
-        existing_source_data_overlap_check = [
-            sd.id
-            for sd in queried_existing_research_context.source_data
-            if sd.id not in queried_new_research_context_source_data_ids
+        extending_source_data_check = [
+            sd_id
+            for sd_id in queried_new_research_context_source_data_ids
+            if sd_id not in queried_existing_source_data_ids
         ]
 
-        assert len(existing_source_data_overlap_check) == 0
+        assert len(extending_source_data_check) != 0
 
         new_source_data_overlap_check = [
             sd_id for sd_id in new_source_data_ids if sd_id not in queried_new_research_context_source_data_ids
@@ -130,7 +138,7 @@ def test_extend_research_context_controller(
     app_initialization_container: ApplicationContainer,
     db_session: TDatabaseFactory,
     fake: Faker,
-    fake_client_with_conversation: SQLAClient,
+    fake_client_with_research_context_and_new_sources: SQLAClient,
     fake_source_data_list: list[SQLAResearchContext],
 ) -> None:
     controller: ExtendResearchContextController = (
@@ -139,29 +147,32 @@ def test_extend_research_context_controller(
 
     assert controller is not None
 
-    client_with_context = fake_client_with_conversation
+    client_with_context = fake_client_with_research_context_and_new_sources
     llm_name = fake.name()
 
-    # Make sure all source data have valid, unique IDs
+    existing_research_context = random.choice(client_with_context.research_contexts)
+    # Populate source data into the existing research context
     existing_source_data_ids = []
-    for context in client_with_context.research_contexts:
-        existing_source_data_ids.extend([sd.id for sd in context.source_data])
+    for source_datum in client_with_context.source_data:
+        existing_research_context.source_data.append(source_datum)
+        existing_source_data_ids.append(source_datum.id)
 
     incoming_source_data_list = fake_source_data_list
     new_source_data_list: List[SQLASourceData] = []
     for source_data in incoming_source_data_list:
         while source_data.id is None or source_data.id in existing_source_data_ids + new_source_data_list:
-            source_data.id = random.randint(2001, 3000)
+            source_data.id = random.randint(1001, 2000)
         new_source_data_list.append(source_data)
+
     client_with_context.source_data.extend(new_source_data_list)
     new_source_data_ids = [sd.id for sd in new_source_data_list]
+    assert new_source_data_ids != []
 
     llm = SQLALLM(
         llm_name=llm_name,
         research_contexts=client_with_context.research_contexts,
     )
 
-    existing_research_context = random.choice(client_with_context.research_contexts)
     # Make titles unique to query later
     existing_research_context_title = f"{existing_research_context.title}-{uuid.uuid4()}"
     existing_research_context.title = existing_research_context_title
@@ -176,6 +187,9 @@ def test_extend_research_context_controller(
         )
 
         assert queried_existing_research_context is not None
+        queried_existing_source_data_list = queried_existing_research_context.source_data
+        queried_existing_source_data_ids = [sd.id for sd in queried_existing_source_data_list]
+        assert queried_existing_source_data_ids != new_source_data_ids
 
         queried_client = session.get(SQLAClient, queried_existing_research_context.client_id)
 
@@ -203,17 +217,19 @@ def test_extend_research_context_controller(
         queried_new_research_context = session.get(SQLAResearchContext, view_model.research_context_id)
 
         assert queried_new_research_context is not None
+        assert queried_new_research_context.title == new_research_context_title
+        assert queried_new_research_context.description == new_research_context_description
 
         queried_new_research_context_source_data_list = queried_new_research_context.source_data
         queried_new_research_context_source_data_ids = [sd.id for sd in queried_new_research_context_source_data_list]
 
-        existing_source_data_overlap_check = [
-            sd.id
-            for sd in queried_existing_research_context.source_data
-            if sd.id not in queried_new_research_context_source_data_ids
+        extending_source_data_check = [
+            sd_id
+            for sd_id in queried_new_research_context_source_data_ids
+            if sd_id not in queried_existing_source_data_ids
         ]
 
-        assert len(existing_source_data_overlap_check) == 0
+        assert len(extending_source_data_check) == 0
 
         new_source_data_overlap_check = [
             sd_id for sd_id in new_source_data_ids if sd_id not in queried_new_research_context_source_data_ids
